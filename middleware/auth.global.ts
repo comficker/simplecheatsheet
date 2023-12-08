d3c417b9a95ff66c3cb5aac8efa9ac153c01d276
comficker <comficker@gmail.com>
2023-12-08 15:36:58 +0700

    Init

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,41 @@
 import {useUserStore} from "~/stores/user";
+import {navigateTo} from "#app";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  if (to.name?.toString() === 'topic') {
+    const redirects: {[key: string]: string[]} = {
+      laravel: [
+        "/laravel-artisan/",
+        "/laravel-auth-cheat-sheet/",
+        "/laravel-blade/",
+        "/laravel-cookie/",
+        "/laravel-db-cheat-sheet/",
+        "/laravel-validation/",
+      ],
+      powershell: [
+        "/powershell-regular-expressions/"
+      ],
+      pl_sql: [
+        "/pl-sql-cursors/",
+        "/pl-sql-date-time/",
+        "/pl-sql-exceptions/",
+        "/pl-sql-triggers/"
+      ],
+      svelte: [
+        '/svelte-store-cheat-sheet/'
+      ]
+    }
+    let redirect;
+    for (let i = 0; i < Object.keys(redirects).length; i++) {
+      const key = Object.keys(redirects)[i]
+      if (redirects[key].indexOf(to.path.toString()) >= 0) {
+        redirect = `/${key.replace("_", "-")}`
+        break;
+      }
+    }
+    if (redirect && redirect != to.path)
+      return navigateTo(redirect);
+  }
   const us = useUserStore()
   if (!us.isLogged) {
     const {$authenticate} = useNuxtApp()
