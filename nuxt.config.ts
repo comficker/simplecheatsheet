a7cfc0e3d9a26e5bc33a08a988b7a50b9107651d
comficker <comficker@gmail.com>
2023-12-21 10:19:34 +0700

    Init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,14 +58,15 @@ export default defineNuxtConfig({
         { name: 'apple-touch-fullscreen', content: 'yes' },
       ],
       script: [
+        {src: 'https://www.googletagmanager.com/gtag/js?id=G-SG8JNET42C', async: true},
         {
           hid: 'gtmHead',
           innerHTML: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MKLR8GDG');`,
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-SG8JNET42C');
+  `,
           //@ts-ignore
           body: true
         },
